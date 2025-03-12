912e4f8c65ac6f550edcf91d4a97f25eea742788
Maurice Goodman <goodman.maurice@gmail.com>
2025-03-12 12:23:31 -0400

    small fix to nu_pred:prey implementation

--- R/dBdt.R
+++ R/dBdt.R
@@ -70,7 +70,7 @@ function( Time,
   Ypred_ij = rep(1,n_species) %*% t(Ypred_j)
   Yprey_i = Bt_i / B_i
   Yprey_ij = Yprey_i %*% t(rep(1,n_species))
-  nu_ij = rep(1,n_species) %*% t(nu_i) + nu_ij
+  nu_ij = rep(1,n_species) %*% t(nu_i) + t(nu_ij)
   # Consumption = Equilibrium * Pred_functional_response * Prey_functional_response
   #Qe_ij = adsparse_to_matrix(Qe_ij)
   Q_ij = Qe_ij * ( X_ij * Ypred_ij / ( X_ij - 1 + Ypred_ij ) ) * Yprey_ij * exp(nu_ij)
